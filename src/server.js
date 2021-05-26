0a57b1d7197a42cb37c6a787ee0ce3ed1f026344
blockchain-nft <serzh.shatohin@bk.ru>
2021-05-26 05:17:05 +0300

    add cors to server

--- src/server.js
+++ src/server.js
@@ -136,6 +136,7 @@ app.use(bodyParser.json());
 // Authentication
 // -----------------------------------------------------------------------------
 app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, Authorization');
   next();
 });
