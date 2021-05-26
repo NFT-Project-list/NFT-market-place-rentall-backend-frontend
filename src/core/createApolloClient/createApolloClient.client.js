4d799848634c745bfac35402a2b785a94efb9e35
blockchain-nft <serzh.shatohin@bk.ru>
2021-05-26 17:02:14 +0300

    fix graphql client uri 3

--- src/core/createApolloClient/createApolloClient.client.js
+++ src/core/createApolloClient/createApolloClient.client.js
@@ -2,7 +2,7 @@ import ApolloClient, { createNetworkInterface } from 'apollo-client';
 
 const client = new ApolloClient({
   networkInterface: createNetworkInterface({
-    uri: 'http://127.0.0.1:3000/graphql',
+    uri: 'https://www.rentluv.com/graphql',
     opts: {
       // Additional fetch options like `credentials` or `headers`
       credentials: 'include'
